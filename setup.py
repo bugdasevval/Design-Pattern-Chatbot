7d87662327e75121667d0a955b794431be1e3634
bugdasevval <sevvalbugda3@gmail.com>
2025-06-06 01:46:07 +0300

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Design Pattern Chatbot',
+    version= '0.0.0',
+    author= 'Sevval Bugda',
+    author_email= 'sevvalbugda3@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
